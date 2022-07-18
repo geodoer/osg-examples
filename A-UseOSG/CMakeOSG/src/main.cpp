aca261d84e2d719c65d41edbffc268f3ad7235b8
geodoer <geodoer@163.com>
2022-07-18 09:54:08 +0800

    feat: CMake与OSG
    Signed-off-by: geodoer <geodoer@163.com>

---
+++ A-UseOSG/CMakeOSG/src/main.cpp
@@ -0,0 +1,13 @@
+﻿#include <osgViewer/Viewer>
+#include <osgDB/ReadFile>
+
+int main()
+{
+	//https://github.com/openscenegraph/OpenSceneGraph-Data
+	std::string data_path = R"(E:\osg\OpenSceneGraph-Data\)";
+	
+	osg::ref_ptr<osgViewer::Viewer> viewer = new osgViewer::Viewer;
+	osg::ref_ptr<osg::Node> node = osgDB::readNodeFile(data_path + "glider.osg");
+	viewer->setSceneData(node.get());
+	return viewer->run();
+}
